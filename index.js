1a1a31c68b0198fbf226101b78cbdc62ee5bdc87
tomo <tomo.mouse.9086@gmail.com>
2018-09-15 10:28:31 +0900

    lifegameバグ取れたよ

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const Launchpad = require("./launchpad.js")
 const Lifegame = require("./lifegame.js")
 
-const launchpad = new Launchpad()
+// const launchpad = new Launchpad()
+const lifegame = new Lifegame(8, 8)
 
-launchpad.on((d, m) => {
-	if(!m[2]) return
-	console.log(String.fromCharCode(m[1]))
-})
-setTimeout(() => {
-	console.log("reset!")
-	launchpad.reset()
-	launchpad.on((d, m) => {
-		if(!m[2]) return
-		console.log(m[1])
-	})
-}, 5000)
+lifegame.setRandom()
 
+// lifegame.field[5][5].alive = true
+// lifegame.field[6][5].alive = true
+// lifegame.field[7][5].alive = true
+// lifegame.field[5][6].alive = true
+// lifegame.field[6][7].alive = true
+
+console.log(lifegame.states + "\n")
+setInterval(() => {
+	lifegame.update()
+	console.log(lifegame.states + "\n")
+}, 500)
+
+// console.log(lifegame.field.map(v => v.map(w => w.around.reduce((p, c) => (c ? 1 : 0) + p, 0)).join(", ")).join("\n"))
 process.on("SIGINT", () => {
 	launchpad.close()
 	process.exit(0)
--- lifegame.js
+++ lifegame.js
@@ -4,8 +4,11 @@ class Lifegame {
 			constructor(x, y) {
 				if(x === undefined) throw new Error(`Cell: argument "x" is required`)
 				if(y === undefined) throw new Error(`Cell: argument "y" is required`)
-				this.alive = false
-				this.nextAlive = false
+				this.coordinate = [null, null]
+				this.x = x
+				this.y = y
+				this.status = false
+				this.nextStatus = false
 				this.around = [null, null, null, null, null, null, null, null]
 			}
 			setAround(upper = null, upperright = null, right = null, lowerright = null, lower = null, lowerleft = null, left = null, upperleft = null) {
@@ -24,38 +27,70 @@ class Lifegame {
 					if(!this.around[i]) continue
 					if(this.around[i].alive) livesaround++
 				}
-				if(this.alive && (livesaround <= 1 || livesaround >= 4)) this.nextAlive = false
-				if(!this.alive && livesaround === 3) this.nextAlive = true
+				this.nextStatus = this.status
+				if(this.status && (livesaround <= 1 || livesaround >= 4)) this.nextStatus = false
+				if(!this.status && livesaround === 3) this.nextStatus = true
 			}
 			update() {
-				this.alive = this.nextAlive
-				this.nextAlive = false
+				this.status = this.nextStatus
+			}
+			set x(val) {
+				this.coordinate[0] = val
+			}
+			get x() {
+				return this.coordinate[0]
+			}
+			set y(val) {
+				this.coordinate[1] = val
+			}
+			get y() {
+				return this.coordinate[1]
+			}
+			set alive(val) {
+				if(typeof val !== "boolean") throw new Error(`Cell.alive: alive must be boolean`)
+				this.status = val
+			}
+			get alive() {
+				return this.status
 			}
 		}
 		this.width = width
 		this.height = height
 		this.field = null
+
+		this.init(this.width, this.height)
 	}
 	init(width, height) {
-		if(width === undefined) throw new Error(`Lifegame: argument "width" is required`)
-		if(height === undefined) throw new Error(`Lifegame: argument "height" is required`)
-		this.field = Array(width).fill(Array(height).fill(null))
+		if(width === undefined) throw new Error(`Lifegame.init(): argument "width" is required`)
+		if(height === undefined) throw new Error(`Lifegame.init(): argument "height" is required`)
+		this.field = new Array(width).fill(null)
 		for(let x in this.field) {
+			this.field[x] = new Array(height).fill(null)
 			for(let y in this.field[x]) {
 				this.field[x][y] = new this.Cell(x, y)
 			}
 		}
 		for(let x in this.field) {
+			x = +x
 			for(let y in this.field[x]) {
+				y = +y
+				const upper = this.field[x][y - 1]
+				const upperright = this.field[x + 1] ? this.field[x + 1][y - 1] : undefined
+				const right = this.field[x + 1] ? this.field[x + 1][y] : undefined
+				const lowerright = this.field[x + 1] ? this.field[x + 1][y + 1] : undefined
+				const lower = this.field[x][y + 1]
+				const lowerleft = this.field[x - 1] ? this.field[x - 1][y + 1] : undefined
+				const left = this.field[x - 1] ? this.field[x - 1][y] : undefined
+				const upperleft = this.field[x - 1] ? this.field[x - 1][y - 1] : undefined
 				this.field[x][y].setAround(
-					this.field[x][y - 1],
-					this.field[x + 1][y - 1],
-					this.field[x + 1][y],
-					this.field[x + 1][y + 1],
-					this.field[x][y + 1],
-					this.field[x - 1][y + 1],
-					this.field[x - 1][y],
-					this.field[x - 1][y - 1])
+					upper,
+					upperright,
+					right,
+					lowerright,
+					lower,
+					lowerleft,
+					left,
+					upperleft)
 			}
 		}
 	}
@@ -71,9 +106,26 @@ class Lifegame {
 			}
 		}
 	}
+	setStatus(x, y, status) {
+		if(x === undefined) throw new Error(`Lifegame.setStatus(): argument "x" is required`)
+		if(y === undefined) throw new Error(`Lifegame.setStatus(): argument "y" is required`)
+		this.field[x][y].alive = status
+		return true
+	}
+	setRandom() {
+		for(let x in this.field) {
+			for(let y in this.field[x]) {
+				this.setStatus(x, y, Boolean((Math.random() * 2) | 0))
+			}
+		}
+	}
 	get states() {
-		if(!this.field) throw new Error(`Lifegame: field is not set`)
-		return this.field.map(v => Number(v.alive))
+		if(!this.field) throw new Error(`Lifegame.states: field is not set`)
+		return this._transpose(this.field).map(v => v.map(w => Number(w.alive)).join(", ")).join("\n")
+		// return this.field.map(v => v.map(w => Number(w.alive)).join(", ")).join("\n")
+	}
+	_transpose(arr) {
+		return arr[0].map((_, c) => arr.map(r => r[c]))
 	}
 }
 
